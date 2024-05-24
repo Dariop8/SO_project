540482ee56249cb2d12a97f682e5b9b4e3fae05c
DarioF <dfinocchiaro903@gmail.com>
2024-05-24 12:00:37 +0200

    implementazione bitmap (ancora da testare)

---
+++ bitmap.c
@@ -0,0 +1,33 @@
+#include "bitmap.h"
+#include <assert.h>
+
+int Bitmap_dim(int n_bits) {
+    // divido per 8 per ottenere i byte necessari, aggiungo 7 per arrotondare per eccesso
+    // (1bit + 7)/ 8 = 1 byte, (8bit + 7)/ 8 = 1byte, (9bit + 7)/ 8 = 2byte ...
+    return (n_bits + 7) / 8;  
+}
+
+Bitmap Bitmap_init(Bitmap* bitmap, char* bits, int n_bits) {
+    bitmap->bits = bits;
+    bitmap->n_bits = n_bits;
+    return *bitmap;
+}
+
+void Bitmap_set(Bitmap* bitmap, int index, int value) {
+    int byte_index = index / 8; //indice del byte all'interno di bits
+    int bit_index = index % 8; // indice del bit all'interno di byte_index
+    assert(byte_index >= 0 && byte_index < Bitmap_dim(bitmap->n_bits)); //indice fuori dai limiti
+    if (value) {
+        bitmap->bits[byte_index] |= (1 << bit_index); // imposto il bit a 1
+    } else {
+        bitmap->bits[byte_index] &= ~(1 << bit_index); // imposto il bit a 0
+    }
+}
+
+int Bitmap_show(Bitmap* bitmap, int index) {
+    int byte_index = index / 8;
+    int bit_index = index % 8;
+    assert(byte_index >= 0 && byte_index < Bitmap_dim(bitmap->n_bits)); //indice fuori dai limiti
+    int ris = (bitmap->bits[byte_index] & (1 << bit_index)) != 0; //valore del bit
+    return ris; 
+}
---
+++ bitmap.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <stdint.h>
+
+typedef struct {
+    char* bits;   // Array di bit
+    int n_bits;     // Numero di bit nella bitmap
+} Bitmap;
+
+//funzione che calcola e restituisce la dimensione in bytes necessaria per memorizzare i bits dati in input
+int Bitmap_dim(int n_bits);
+
+//funzione che inizializza la bitmap con la dimensione specificata
+Bitmap Bitmap_init(Bitmap* bitmap, char* bits, int n_bits);
+
+//funzione che imposta un bit specifico nella bitmap ad un valore value (0 o 1)
+void Bitmap_set(Bitmap* bitmap, int index, int value);
+
+//funzione che restituisce il valore (0 o 1) di un bit specifico della bitmap
+int Bitmap_show(Bitmap* bitmap, int index);
